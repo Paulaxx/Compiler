ddb12044db50e9f5587079ac23c071f80eeb4a9a
Paulaxx <palinkagr@gmail.com>
2021-01-05 18:34:32 +0100

    Some fixes

--- Expression.py
+++ Expression.py
@@ -1,3 +1,9 @@
 class Expression:
-    def __init__(self, register):
+    def __init__(self, register, counter):
         self.register = register
+        self.counter = counter
+
+
+class Command:
+    def __init__(self, counter):
+        self.counter = counter
